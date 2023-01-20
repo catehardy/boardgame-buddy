bc13186f4d5401fb40c47ad4eec54a560678b431
Catherine Hardy <83373032+catehardy@users.noreply.github.com>
2023-01-20 15:32:51 +0000

    adjust search button position to be within focus outline

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
 function displayResult() {
-    const input = document.getElementById("boardgame-searchfield").value;
-    if (input) {
-        // Remove previous results from display
-        hideSearchDropdown();
-        let parent = document.getElementById("api-spot");
-        empty(parent);
-        
-        lookupUserInput()
-        .then((response) => {
-            const gameData = response;
-            console.log("game data: ", gameData); // TODO: remove this log once search feature completed
-            const apiDiv = document.querySelector("#api-spot");
-            const inputVal = document.getElementById("boardgame-searchfield").value;
-            const header = document.createElement("h2");
-            if (!gameData.count) {
-              header.innerText = `Sorry, no results for "${inputVal}"`;
-              header.classList.add("purple-text-block");
-              apiDiv.appendChild(header);
-            } else {
-              header.innerText = `Results for "${inputVal}"`;
-              header.classList.add("purple-text-block");
-              apiDiv.appendChild(header);
-              
-              // Display results of game search in dropdown menu
-              for (let i = 0; i < gameData.games.length; i++) {
-                  const option = gameData.games[i].name;
-                  const addOption = document.createElement("p");
-                  addOption.innerText = option;
-                  apiDiv.appendChild(link(option, gameData.games[i].url));
-              }
-            }
-            showSearchDropdown();
-        })
-        .catch((error) => console.error(error));
-    }
+  const input = document.getElementById("boardgame-searchfield").value;
+  if (input) {
+    // Remove previous results from display
+    hideSearchDropdown();
+    let parent = document.getElementById("api-spot");
+    empty(parent);
+
+    lookupUserInput()
+      .then((response) => {
+        const gameData = response;
+        console.log("game data: ", gameData); // TODO: remove this log once search feature completed
+        const apiDiv = document.querySelector("#api-spot");
+        const inputVal = document.getElementById("boardgame-searchfield").value;
+        const header = document.createElement("h2");
+        if (!gameData.count) {
+          header.innerText = `Sorry, no results for "${inputVal}"`;
+          header.classList.add("purple-text-block");
+          apiDiv.appendChild(header);
+        } else {
+          header.innerText = `Results for "${inputVal}"`;
+          header.classList.add("purple-text-block");
+          apiDiv.appendChild(header);
+
+          // Display results of game search in dropdown menu
+          for (let i = 0; i < gameData.games.length; i++) {
+            const option = gameData.games[i].name;
+            const addOption = document.createElement("p");
+            addOption.innerText = option;
+            apiDiv.appendChild(link(option, gameData.games[i].url));
+          }
+        }
+        showSearchDropdown();
+      })
+      .catch((error) => console.error(error));
+  }
 }
 
 function lookupUserInput() {
@@ -49,20 +49,20 @@ function lookupUserInput() {
 }
 
 function empty(element) {
-    element.replaceChildren();
+  element.replaceChildren();
 }
 
 // manage search dropdown visibility
 showSearchDropdown = () =>
-document.getElementById("search-dropdown").classList.add("show");
+  document.getElementById("search-dropdown").classList.add("show");
 hideSearchDropdown = () =>
-document.getElementById("search-dropdown").classList.remove("show");
+  document.getElementById("search-dropdown").classList.remove("show");
 // hide search dropdown if user clicks away
-window.onclick = function(e) {
-    if (!e.target.matches(".dropdown-content")) {
-        hideSearchDropdown();
-    }
-}
+window.onclick = function (e) {
+  if (!e.target.matches(".dropdown-content")) {
+    hideSearchDropdown();
+  }
+};
 
 // create text links - used for search dropdown
 function link(text, href) {
